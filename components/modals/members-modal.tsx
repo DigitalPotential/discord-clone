10a02a3ee7edbdd5eefb0f3dea34603b836e01cb
DigitalPotential <Carlos@digitalpotential.se>
2023-09-03 18:03:52 +0200

    feat: onKick, onRoleChange & added query-string

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -1,5 +1,21 @@
 "use client";
 
+import axios from "axios";
+import qs from "query-string";
+import { 
+  Check,
+  Gavel,
+  Loader2,
+  MoreVertical, 
+  Shield, 
+  ShieldAlert, 
+  ShieldCheck,
+  ShieldQuestion
+} from "lucide-react";
+import { useState } from "react";
+import { MemberRole } from "@prisma/client";
+import { useRouter } from "next/navigation";
+
 import {
   Dialog,
   DialogContent,
@@ -9,11 +25,8 @@ import {
 } from "@/components/ui/dialog";
 import { useModal } from "@/hooks/use-modal-store";
 import { ServerWithMembersWithProfiles } from "@/types";
-import { ScrollArea } from "../ui/scroll-area";
-import { UserAvatar } from "../user-avatar";
-import { Check, Gavel, Loader2, MoreVertical, Shield, ShieldAlert, ShieldCheck, ShieldQuestion } from "lucide-react";
-import { useState } from "react";
-
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { UserAvatar } from "@/components/user-avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -23,8 +36,8 @@ import {
   DropdownMenuSub,
   DropdownMenuSubContent,
   DropdownMenuTrigger,
-  DropdownMenuSubTrigger
-} from "@/components/ui/dropdown-menu"
+  DropdownMenuSubTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const roleIconMap = {
   "GUEST": null,
@@ -33,11 +46,54 @@ const roleIconMap = {
 }
 
 export const MembersModal = () => {
+  const router = useRouter();
   const { onOpen, isOpen, onClose, type, data } = useModal();
   const [loadingId, setLoadingId] = useState("");
 
   const isModalOpen = isOpen && type === "members";
   const { server } = data as { server: ServerWithMembersWithProfiles };
+
+  const onKick = async (memberId: string) => {
+    try {
+      setLoadingId(memberId);
+      const url = qs.stringifyUrl({
+        url: `/api/members/${memberId}`,
+        query: {
+          serverId: server?.id,
+        },
+      });
+
+      const response = await axios.delete(url);
+
+      router.refresh();
+      onOpen("members", { server: response.data });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoadingId("");
+    }
+  }
+
+  const onRoleChange = async (memberId: string, role: MemberRole) => {
+    try {
+      setLoadingId(memberId);
+      const url = qs.stringifyUrl({
+        url: `/api/members/${memberId}`,
+        query: {
+          serverId: server?.id,
+        }
+      });
+
+      const response = await axios.patch(url, { role });
+
+      router.refresh();
+      onOpen("members", { server: response.data });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoadingId("");
+    }
+  }
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -46,7 +102,9 @@ export const MembersModal = () => {
           <DialogTitle className="text-2xl text-center font-bold">
             Manage Members
           </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500">
+          <DialogDescription 
+            className="text-center text-zinc-500"
+          >
             {server?.members?.length} Members
           </DialogDescription>
         </DialogHeader>
@@ -75,31 +133,41 @@ export const MembersModal = () => {
                           className="flex items-center"
                         >
                           <ShieldQuestion
-                            className="h-4 w-4 mr-2"
+                            className="w-4 h-4 mr-2"
                           />
                           <span>Role</span>
                         </DropdownMenuSubTrigger>
                         <DropdownMenuPortal>
                           <DropdownMenuSubContent>
-                            <DropdownMenuItem>
+                            <DropdownMenuItem
+                              onClick={() => onRoleChange(member.id, "GUEST")}
+                            >
                               <Shield className="h-4 w-4 mr-2" />
                               Guest
                               {member.role === "GUEST" && (
-                                <Check className="h-4 w-4 ml-auto" />
+                                <Check
+                                  className="h-4 w-4 ml-auto"
+                                />
                               )}
                             </DropdownMenuItem>
-                            <DropdownMenuItem>
+                            <DropdownMenuItem
+                              onClick={() => onRoleChange(member.id, "MODERATOR")}
+                            >
                               <ShieldCheck className="h-4 w-4 mr-2" />
                               Moderator
                               {member.role === "MODERATOR" && (
-                                <Check className="h-4 w-4 ml-auto" />
+                                <Check
+                                  className="h-4 w-4 ml-auto"
+                                />
                               )}
                             </DropdownMenuItem>
                           </DropdownMenuSubContent>
                         </DropdownMenuPortal>
                       </DropdownMenuSub>
                       <DropdownMenuSeparator />
-                      <DropdownMenuItem>
+                      <DropdownMenuItem
+                        onClick={() => onKick(member.id)}
+                      >
                         <Gavel className="h-4 w-4 mr-2" />
                         Kick
                       </DropdownMenuItem>
@@ -108,12 +176,14 @@ export const MembersModal = () => {
                 </div>
               )}
               {loadingId === member.id && (
-                <Loader2 className="animate-spin text-zinc-500 ml-auto w-4 h-4" />
+                <Loader2
+                  className="animate-spin text-zinc-500 ml-auto w-4 h-4"
+                />
               )}
             </div>
           ))}
         </ScrollArea>
       </DialogContent>
     </Dialog>
-  );
-};
+  )
+}
