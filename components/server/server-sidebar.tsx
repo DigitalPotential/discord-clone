4410674b87a14bdbd8cf648f3d636299e3578804
DigitalPotential <Carlos@digitalpotential.se>
2023-09-05 20:00:47 +0200

    feat: ChannelList, ServerSection WIP

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -7,6 +7,8 @@ import { ServerHeader } from "@/components/server/server-header";
 import { ScrollArea } from "../ui/scroll-area";
 import { ServerSearch } from "./server-search";
 import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+import { Separator } from "../ui/separator";
+import { ServerSection } from "./server-section";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -119,6 +121,18 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
             ]}
           />
         </div>
+        <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
+        {!!textChannels?.length && (
+          <div className="mb-2">
+            <ServerSection
+              sectionType="channels"
+              channelType={ChannelType.TEXT}
+              role={role}
+              label="Text Channels"
+            />
+            
+          </div>
+        )}
       </ScrollArea>
     </div>
   );
