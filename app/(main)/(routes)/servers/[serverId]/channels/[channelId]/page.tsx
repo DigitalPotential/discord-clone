3402ece4ddc28b85c35727e28567a10832e0726e
DigitalPotential <Carlos@digitalpotential.se>
2023-09-06 07:44:52 +0200

    feat: ChannelId Page, MemberId Page

---
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -0,0 +1,12 @@
+
+
+const ChannelIdPage = () => {
+
+  return ( 
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      Channel id Page
+    </div>
+   );
+}
+ 
+export default ChannelIdPage;
